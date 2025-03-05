08d21633b97ef677ce0ad827e78b87c32636963d
Rotimi Oluwatofiyan <rotimioluwatophiyan@gmail.com>
2025-03-05 15:19:12 +0100

    strike and underline support

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,26 @@
 export enum UnicodeStyle {
-    NORMAL = 'NORMAL',
-    MATH_BOLD = 'MATH_BOLD',
-    MATH_ITALIC = 'MATH_ITALIC',
-    MATH_BOLD_ITALIC = 'MATH_BOLD_ITALIC',
-    MONOSPACE = 'MONOSPACE',
-    FULLWIDTH = 'FULLWIDTH',
-    SANS_SERIF = 'SANS_SERIF',
-    SANS_SERIF_BOLD = 'SANS_SERIF_BOLD',
-    SANS_SERIF_ITALIC = 'SANS_SERIF_ITALIC'
+  NORMAL = "NORMAL",
+  MATH_BOLD = "MATH_BOLD",
+  MATH_ITALIC = "MATH_ITALIC",
+  MATH_UNDERLINE = "MATH_UNDERLINE",
+  MATH_STRIKE = "MATH_STRIKE",
+  MATH_BOLD_ITALIC = "MATH_BOLD_ITALIC",
+  MONOSPACE = "MONOSPACE",
+  FULLWIDTH = "FULLWIDTH",
+  SANS_SERIF = "SANS_SERIF",
+  SANS_SERIF_BOLD = "SANS_SERIF_BOLD",
+  SANS_SERIF_ITALIC = "SANS_SERIF_ITALIC",
 }
 
 export interface UnicodeMap {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 export type StyleMap = Record<UnicodeStyle, UnicodeMap>;
 
 export class UnicodeConversionError extends Error {
-    constructor(message: string) {
-        super(message);
-        this.name = 'UnicodeConversionError';
-    }
-} 
+  constructor(message: string) {
+    super(message);
+    this.name = "UnicodeConversionError";
+  }
+}
